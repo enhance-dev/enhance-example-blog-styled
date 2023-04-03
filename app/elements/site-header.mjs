109653be254a28dc47774f1e46e76799175137cc
Cole Peters <cole@colepeters.com>
2023-04-03 15:22:03 -0500

    Update site header styles

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -1,14 +1,27 @@
 export default function SiteHeader ({ html }) {
   return html`
+    <style>
+      header {
+        background-color: hsla(0deg 0% 0% / 10%);
+      }
+
+      @media (prefers-color-scheme: dark) {
+        header {
+          background-color: hsla(0deg 0% 100% / 10%);
+        }
+      }
+    </style>
     <header>
-      <h1 class='text2 text4-lg font-heading font-bold tracking-1 pt4 pt6-lg pb0 pb2-lg'>
-        <a href='/' class='no-underline'>
-          Enhance Blog Template
-        </a>
-      </h1>
-      <p class='font-body pb4 pb6-lg'>
-        A subtitle for this blog
-      </p>
+      <site-container class='pl0 pr0 pl4-lg pr4-lg'>
+        <h1 class='text2 text4-lg font-heading font-bold tracking-1 pt4 pt6-lg pb0 pb2-lg'>
+          <a href='/' class='no-underline'>
+            Enhance Blog Template
+          </a>
+        </h1>
+        <p class='font-body pb4 pb6-lg'>
+          A subtitle for this blog
+        </p>
+      </site-container>
     </header>
   `
 }
--- app/elements/site-layout.mjs
+++ app/elements/site-layout.mjs
@@ -1,7 +1,7 @@
 export default function SiteLayout ({ html }) {
   return html`
+    <site-header></site-header>
     <site-container class='pl0 pr0 pl4-lg pr4-lg'>
-      <site-header></site-header>
         <slot></slot>
       <site-footer></site-footer>
     </site-container>
